b5d079e7f01b1e629feb8be1833deca8dcb9b752
shamsisadr01 <shamsisadr01@gmail.com>
2024-09-24 18:27:19 +0330

    Create Seller Aggregate

---
+++ Shop/Shop.Domain/Seller Aggregate/Services/ISellerDomainService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1.Shop.Domain.Seller_Aggregate.Services
+{
+	public interface ISellerDomainService
+	{
+		bool IsValidSellerInformation(Seller seller);
+
+		bool NationalCodeExistInDataBase(string nationalCode);
+	}
+}
