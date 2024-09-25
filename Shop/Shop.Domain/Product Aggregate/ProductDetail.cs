d26c0642d9da588e3923a042783bfe1d03b5b006
shamsisadr01 <shamsisadr01@gmail.com>
2024-09-25 10:34:31 +0330

    Create Product Aggregate

---
+++ Shop/Shop.Domain/Product Aggregate/ProductDetail.cs
@@ -0,0 +1,23 @@
+﻿using Common.Domain;
+using Common.Domain.Exceptions;
+
+namespace _1.Shop.Domain.Product_Aggregate
+{
+	public class ProductDetail : BaseEntity
+	{
+		public ProductDetail(string key, string value)
+		{
+			NullOrEmptyDomainDataException.CheckString(key, nameof(key));
+			NullOrEmptyDomainDataException.CheckString(value, nameof(value));
+
+			Key = key;
+			Value = value;
+		}
+
+		public long ProductId { get; internal set; }
+
+		public string Key { get; private set; }
+
+        public string Value { get; private set; }
+    }
+}
