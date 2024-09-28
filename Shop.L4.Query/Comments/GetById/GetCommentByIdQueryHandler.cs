48cb997f5ea2dba41ac340077451bcf8ea7838f7
shamsisadr01 <shamsisadr01@gmail.com>
2024-09-28 19:15:23 +0330

    Comment Query

---
+++ Shop.L4.Query/Comments/GetById/GetCommentByIdQueryHandler.cs
@@ -0,0 +1,24 @@
+﻿using Common.L4.Query;
+using Microsoft.EntityFrameworkCore;
+using Shop.L3.Infrastructure.Persistent.Ef;
+using Shop.L4.Query.Comments.DTOs;
+
+namespace Shop.L4.Query.Comments.GetById
+{
+	public class GetCommentByIdQueryHandler : IQueryHandler<GetCommentByIdQuery, CommentDto?>
+	{
+		private readonly StoreContext _context;
+
+		public GetCommentByIdQueryHandler(StoreContext context)
+		{
+			_context = context;
+		}
+
+		public async Task<CommentDto?> Handle(GetCommentByIdQuery request, CancellationToken cancellationToken)
+		{
+			var comment = await _context.Comments.FirstOrDefaultAsync(f => f.Id == request.commentId, cancellationToken: cancellationToken);
+
+			return comment.Map();
+		}
+	}
+}
